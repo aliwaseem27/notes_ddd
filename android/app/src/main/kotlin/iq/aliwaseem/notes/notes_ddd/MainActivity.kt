a8aed49ab18c8467e8ecc5b976e0e9b92c9e3d4c
ali.waseem <ali.waseem27@gmail.com>
2023-06-09 12:30:59 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/iq/aliwaseem/notes/notes_ddd/MainActivity.kt
@@ -0,0 +1,6 @@
+package iq.aliwaseem.notes.notes_ddd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:notes_ddd/domain/auth/auth_failure.dart';
+import 'package:notes_ddd/domain/auth/i_auth_facade.dart';
+import 'package:notes_ddd/domain/auth/value_objects.dart';
+
+part 'sign_in_form_event.dart';
+
+part 'sign_in_form_state.dart';
+
+part 'sign_in_form_bloc.freezed.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacede;
+
+  SignInFormBloc(this._authFacede) : super(SignInFormState.initial()) {
+    on<SignInFormEvent>((event, emit) {
+      event.when(
+        emailChanged: (emailStr) {
+          emit(state.copyWith(
+            emailAddress: EmailAddress(emailStr),
+            authFailureOrSuccessOption: none(),
+          ));
+        },
+        passwordChanged: (passwordStr) {
+          emit(state.copyWith(
+            password: Password(passwordStr),
+            authFailureOrSuccessOption: none(),
+          ));
+        },
+        registerWithEmailAndPasswordPressed:
+            _performActionOnAuthFacadeWithEmailAndPassword(
+          _authFacede.registerWithEmailAndPassword,
+        ),
+        signInWithEmailAndPasswordPressed:
+            _performActionOnAuthFacadeWithEmailAndPassword(
+          _authFacede.signInWithEmailAndPassword,
+        ),
+        signInWithGooglePressed: () async {
+          emit(state.copyWith(
+            isSubmitting: true,
+            authFailureOrSuccessOption: none(),
+          ));
+          final failureOrSuccess = await _authFacede.signInWithGoogle();
+          emit(state.copyWith(
+            isSubmitting: false,
+            authFailureOrSuccessOption: some(failureOrSuccess),
+          ));
+        },
+      );
+    });
+  }
+
+  _performActionOnAuthFacadeWithEmailAndPassword(
+      Future<Either<AuthFailure, Unit>> Function({
+    required EmailAddress emailAddress,
+    required Password password,
+  })
+          forwardedCall) async {
+    Either<AuthFailure, Unit>? failureOrSuccess;
+    final isEmailValid = state.emailAddress.isValid();
+    final isPasswordValid = state.password.isValid();
+    if (isEmailValid && isPasswordValid) {
+      emit(state.copyWith(
+        isSubmitting: true,
+        authFailureOrSuccessOption: none(),
+      ));
+      failureOrSuccess = await forwardedCall(
+        emailAddress: state.emailAddress,
+        password: state.password,
+      );
+    }
+    emit(state.copyWith(
+      isSubmitting: false,
+      showErrorMessages: true,
+      authFailureOrSuccessOption: optionOf(failureOrSuccess),
+    ));
+  }
+}
