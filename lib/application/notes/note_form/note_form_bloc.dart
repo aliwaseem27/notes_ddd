4dff3110efac1168a042c045969e5b52ab6efc76
ali.waseem <ali.waseem27@gmail.com>
2023-07-02 19:25:17 +0300

    add: note form bloc

--- lib/application/notes/note_form/note_form_bloc.dart
+++ lib/application/notes/note_form/note_form_bloc.dart
@@ -6,6 +6,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:kt_dart/collection.dart';
 import 'package:notes_ddd/domain/notes/note.dart';
 import 'package:notes_ddd/domain/notes/note_failure.dart';
+import 'package:notes_ddd/domain/notes/value_objects.dart';
 import 'package:notes_ddd/presentation/notes/note_form/misc/todo_item_presentation_classes.dart';
 
 part 'note_form_event.dart';
@@ -16,8 +17,46 @@ part "note_form_bloc.freezed.dart";
 
 class NoteFormBloc extends Bloc<NoteFormEvent, NoteFormState> {
   NoteFormBloc() : super(NoteFormState.initial()) {
-    on<NoteFormEvent>((event, emit) {
-      // TODO: implement event handler
+    on<_Initialized>((event, emit) {
+      emit(event.initialNoteOption.fold(
+        () => state,
+        (initialNote) => state.copyWith(
+          note: initialNote,
+          isEditing: true,
+        ),
+      ));
+    });
+
+    on<_BodyChanged>((event, emit) {
+      emit(state.copyWith(
+        note: state.note.copyWith(body: NoteBody(event.bodyStr)),
+        saveFailureOrSuccessOption: none(),
+      ));
+    });
+
+    on<_ColorChanged>((event, emit) {
+      emit(state.copyWith(
+        note: state.note.copyWith(color: NoteColor(event.color)),
+        saveFailureOrSuccessOption: none(),
+      ));
+    });
+
+    on<_TodosChanged>((event, emit) {
+      emit(state.copyWith(
+        note: state.note.copyWith(
+            todos: List3(event.todos.map((primitive) => primitive.toDomain()))),
+        saveFailureOrSuccessOption: none(),
+      ));
+    });
+
+    on<_Saved>((event, emit){
+      emit(state.copyWith(
+        isSaving: true,
+        saveFailureOrSuccessOption: none(),
+      ));
+      // if (state.note.failureOption.isNone()){
+      //
+      // }
     });
   }
 }
