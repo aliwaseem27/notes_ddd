836c0fd07b58c2debe1bf342648f77de61960ced
ali.waseem <ali.waseem27@gmail.com>
2023-07-03 03:07:28 +0300

    add: NoteOverview Bloc providers and listeners

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -9,10 +9,10 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:cloud_firestore/cloud_firestore.dart' as _i10;
+import 'package:cloud_firestore/cloud_firestore.dart' as _i5;
 import 'package:firebase_auth/firebase_auth.dart' as _i4;
 import 'package:get_it/get_it.dart' as _i1;
-import 'package:google_sign_in/google_sign_in.dart' as _i5;
+import 'package:google_sign_in/google_sign_in.dart' as _i6;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:notes_ddd/application/auth/auth_bloc.dart' as _i15;
 import 'package:notes_ddd/application/auth/sign_in_form/sign_in_form_bloc.dart'
@@ -23,12 +23,12 @@ import 'package:notes_ddd/application/notes/note_form/note_form_bloc.dart'
     as _i12;
 import 'package:notes_ddd/application/notes/note_watcher/note_watcher_bloc.dart'
     as _i13;
-import 'package:notes_ddd/domain/auth/i_auth_facade.dart' as _i6;
-import 'package:notes_ddd/domain/notes/i_note_repository.dart' as _i8;
-import 'package:notes_ddd/infrastructure/auth/firebase_auth_facade.dart' as _i7;
+import 'package:notes_ddd/domain/auth/i_auth_facade.dart' as _i7;
+import 'package:notes_ddd/domain/notes/i_note_repository.dart' as _i9;
+import 'package:notes_ddd/infrastructure/auth/firebase_auth_facade.dart' as _i8;
 import 'package:notes_ddd/infrastructure/core/firebase_injectable_module.dart'
     as _i16;
-import 'package:notes_ddd/infrastructure/notes/note_repository.dart' as _i9;
+import 'package:notes_ddd/infrastructure/notes/note_repository.dart' as _i10;
 import 'package:notes_ddd/presentation/routes/app_router.dart' as _i3;
 
 extension GetItInjectableX on _i1.GetIt {
@@ -46,23 +46,25 @@ extension GetItInjectableX on _i1.GetIt {
     gh.lazySingleton<_i3.AppRouter>(() => _i3.AppRouter());
     gh.lazySingleton<_i4.FirebaseAuth>(
         () => firebaseInjectableModule.firebaseAuth);
-    gh.lazySingleton<_i5.GoogleSignIn>(
+    gh.lazySingleton<_i5.FirebaseFirestore>(
+        () => firebaseInjectableModule.firestore);
+    gh.lazySingleton<_i6.GoogleSignIn>(
         () => firebaseInjectableModule.googleSignIn);
-    gh.lazySingleton<_i6.IAuthFacade>(() => _i7.FirebaseAuthFacade(
+    gh.lazySingleton<_i7.IAuthFacade>(() => _i8.FirebaseAuthFacade(
           gh<_i4.FirebaseAuth>(),
-          gh<_i5.GoogleSignIn>(),
+          gh<_i6.GoogleSignIn>(),
         ));
-    gh.lazySingleton<_i8.INoteRepository>(
-        () => _i9.NoteRepository(gh<_i10.FirebaseFirestore>()));
+    gh.lazySingleton<_i9.INoteRepository>(
+        () => _i10.NoteRepository(gh<_i5.FirebaseFirestore>()));
     gh.factory<_i11.NoteActorBloc>(
-        () => _i11.NoteActorBloc(gh<_i8.INoteRepository>()));
+        () => _i11.NoteActorBloc(gh<_i9.INoteRepository>()));
     gh.factory<_i12.NoteFormBloc>(
-        () => _i12.NoteFormBloc(gh<_i8.INoteRepository>()));
+        () => _i12.NoteFormBloc(gh<_i9.INoteRepository>()));
     gh.factory<_i13.NoteWatcherBloc>(
-        () => _i13.NoteWatcherBloc(gh<_i8.INoteRepository>()));
+        () => _i13.NoteWatcherBloc(gh<_i9.INoteRepository>()));
     gh.factory<_i14.SignInFormBloc>(
-        () => _i14.SignInFormBloc(gh<_i6.IAuthFacade>()));
-    gh.factory<_i15.AuthBloc>(() => _i15.AuthBloc(gh<_i6.IAuthFacade>()));
+        () => _i14.SignInFormBloc(gh<_i7.IAuthFacade>()));
+    gh.factory<_i15.AuthBloc>(() => _i15.AuthBloc(gh<_i7.IAuthFacade>()));
     return this;
   }
 }
--- lib/presentation/notes/notes_overview/notes_overview_page.dart
+++ lib/presentation/notes/notes_overview/notes_overview_page.dart
@@ -1,5 +1,13 @@
+import 'package:another_flushbar/flushbar_helper.dart';
 import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_ddd/application/auth/auth_bloc.dart';
+import 'package:notes_ddd/application/notes/note_actor/note_actor_bloc.dart';
+import 'package:notes_ddd/application/notes/note_watcher/note_watcher_bloc.dart';
+import 'package:notes_ddd/injection.dart';
+import 'package:notes_ddd/presentation/routes/app_router.dart';
 
 @RoutePage()
 class NotesOverviewPage extends StatefulWidget {
@@ -12,8 +20,69 @@ class NotesOverviewPage extends StatefulWidget {
 class _NotesOverviewPageState extends State<NotesOverviewPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Notes overview page"),),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<NoteWatcherBloc>(
+            create: (context) => getIt<NoteWatcherBloc>()
+              ..add(const NoteWatcherEvent.watchAllStarted())),
+        BlocProvider<NoteActorBloc>(
+            create: (context) => getIt<NoteActorBloc>()),
+      ],
+      child: MultiBlocListener(
+        listeners: [
+          BlocListener<AuthBloc, AuthState>(listener: (context, state) {
+            state.maybeMap(
+                unauthenticated: (_) =>
+                    context.router.replace(const SignInRoute()),
+                orElse: () {});
+          }),
+          BlocListener<NoteActorBloc, NoteActorState>(
+              listener: (context, state) {
+            state.maybeMap(
+                deleteFailure: (failureState) {
+                  FlushbarHelper.createError(
+                    duration: const Duration(seconds: 5),
+                    message: failureState.noteFailure.map(
+                      unexpected: (_) =>
+                          "Unexpected error occurred while deleting, please contact support",
+                      insufficientPermission: (_) =>
+                          "Insufficient permissions X",
+                      unableToUpdate: (_) => "Impossible error",
+                    ),
+                  ).show(context);
+                },
+                orElse: () {});
+          }),
+        ],
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Notes"),
+            leading: IconButton(
+              icon: const Icon(Icons.exit_to_app),
+              onPressed: () {
+                context.read<AuthBloc>().add(const AuthEvent.signedOut());
+              },
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  // TODO: Toggle between completed and uncompleted tasks
+                },
+                icon: Icon(Icons.indeterminate_check_box),
+              ),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              // TODO: Navigate to NoteFormPage
+            },
+            child: Icon(Icons.add),
+          ),
+          body: Center(
+            child: Text("Notes overview page"),
+          ),
+        ),
+      ),
     );
   }
 }
